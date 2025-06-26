a393bab64fdc2f0e722767bc448a588b60a884b8
HoangDilo <duriustudio@gmail.com>
2025-06-26 23:18:11 +0700

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Checker Viet Hai Cham Khong API')
+    .setDescription(
+      'The API documentation for Checker Viet Hai Cham Khong application',
+    )
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
